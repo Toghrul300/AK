37cea2637dba8b43d9940c86f9a77533d67e93ea
toghrul <toghrul.au@gmail.com>
2020-03-26 21:18:36 +0400

    1

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
 function onlyNumbers(keyinput) {
   let input = keyinput.charCode ? keyinput.charCode : keyinput.keyCode;
   // если не цифра и удаление
+  // 8 backspace, 46 delete, 109 subtract, 110 decimal, 188 comma, 189 dash, 190 period
   if (input != 8 && input != 46) {
     if (input < 48 || input > 57) {
       return false;
     }
   }
 }
+function toCalculate(startingbalance, payment, interestrate, days) {
+  let months = days / 30;
+  for (i = 1; i <= months; i++) {
+    startingbalance += (startingbalance * interestrate) / 1200;
+    startingbalance += payment;
+  }
+  return startingbalance.toFixed(0);
+}
 function mySavings() {
-  // обнуляй эррор
+  // обнуление результата валидатора
   document.getElementById("balanceError").innerHTML = "";
   document.getElementById("paymentError").innerHTML = "";
-  // добавил пеймент
   document.getElementById("rateError").innerHTML = "";
   document.getElementById("MonthsError").innerHTML = "";
-
-  // let checkBalance = document.savingscalc.balance.value;
-  // проверка валидности
   if (
+    // проверка валидности
     document.savingscalc.balance.value == null ||
     document.savingscalc.balance.value.length == 0 ||
     isNaN(document.savingscalc.balance.value) == true
   ) {
     document.getElementById("finalBalance").innerHTML =
       "Please enter the missing information.";
-    document.getElementById("balanceError").innerHTML =
-      "Numeric value required. Example: 10000";
   } else if (
     document.savingscalc.payment.value == null ||
     document.savingscalc.payment.value.length == 0 ||
@@ -33,8 +37,6 @@ function mySavings() {
   ) {
     document.getElementById("payment").innerHTML =
       "Please enter the missing information.";
-    document.getElementById("paymentError").innerHTML =
-      "Numeric value required. Example: 10000";
   } else if (
     document.savingscalc.rate.value == null ||
     document.savingscalc.rate.value.length == 0 ||
@@ -42,8 +44,6 @@ function mySavings() {
   ) {
     document.getElementById("finalBalance").innerHTML =
       "Please enter the missing information.";
-    document.getElementById("rateError").innerHTML =
-      "Numeric value required. Example: 3.5";
   } else if (
     document.savingscalc.Months.value == null ||
     document.savingscalc.Months.value.length == 0 ||
@@ -51,40 +51,14 @@ function mySavings() {
   ) {
     document.getElementById("finalBalance").innerHTML =
       "Please enter the missing information.";
-    document.getElementById("MonthsError").innerHTML =
-      "Numeric value required. Example: 10";
   } else {
-    function toCalculate(startingbalance, payment, interestrate, days) {
-      let months = days / 30;
-      let result = 0;
-      for (i = 1; i < months; i++) {
-        startingbalance += (startingbalance * interestrate) / 1200;
-        startingbalance += payment;
-      }
-      result = startingbalance;
-      return result.toFixed(2);
-    }
-    // задать переменные данных
     let startingbalance = +document.savingscalc.balance.value;
     let payment = +document.savingscalc.payment.value;
     let interestrate = +document.savingscalc.rate.value;
     let days = +document.savingscalc.Months.value;
     let result = toCalculate(startingbalance, payment, interestrate, days);
-    // калькуляция и выдача баланса
     document.getElementById(
       "finalBalance"
-    ).innerHTML = `Final Balance: ${result}`;
+    ).innerHTML = `your final balance: <b>${result}</b>  AZN`; // калькуляция и выдача баланса
   }
-}
-
-function mySavingsReset() {
-  // ресет всех полей
-  document.getElementById("finalBalance").innerHTML = "Values reset";
-  document.getElementById("balanceError").innerHTML = "";
-  document.getElementById("rateError").innerHTML = "";
-  document.getElementById("MonthsError").innerHTML = "";
-  document.savingscalc.balance.value = null;
-  document.savingscalc.rate.value = null;
-  document.savingscalc.Months.value = null;
-  document.savingscalc.payment.value = null;
 }
